3e71a54bc1ec5ac86968e4295dc5178ea158b4dd
Brian Griggs <brian@bigmissionmotorsports.com>
2025-02-08 21:33:26 -0700

    Add project files.

---
+++ RedMist.TimingAndScoringService/Hubs/TimingAndScoringHub.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RedMist.TimingAndScoringService.Hubs;
+
+public class TimingAndScoringHub : Hub
+{
+    private ILogger Logger { get; }
+
+    public TimingAndScoringHub(ILoggerFactory loggerFactory)
+    {
+        Logger = loggerFactory.CreateLogger(GetType().Name);
+    }
+
+    public async override Task OnConnectedAsync()
+    {
+        await base.OnConnectedAsync();
+        Logger.LogInformation("Client connected: {0}", Context.ConnectionId);
+    }
+
+    public async override Task OnDisconnectedAsync(Exception? exception)
+    {
+        await base.OnDisconnectedAsync(exception);
+        Logger.LogInformation("Client disconnected: {0}", Context.ConnectionId);
+    }
+
+    /// <summary>
+    /// Receives a sent message from a RMonitor relay.
+    /// </summary>
+    /// <param name="command">RMonitor command string</param>
+    /// <returns></returns>
+    /// <see cref="https://github.com/bradfier/rmonitor/blob/master/docs/RMonitor%20Timing%20Protocol.pdf"/>
+    public Task Send(string command)
+    {
+        Logger.LogDebug("RX: {0}", command);
+        return Task.CompletedTask;
+    }
+}
---
+++ RedMist.TimingAndScoringService/Program.cs
@@ -0,0 +1,130 @@
+
+using BigMission.TestHelpers;
+using HealthChecks.UI.Client;
+using Keycloak.AuthServices.Authentication;
+using Keycloak.AuthServices.Authorization;
+using Keycloak.AuthServices.Common;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
+using NLog.Extensions.Logging;
+using RedMist.TimingAndScoringService.Hubs;
+using StackExchange.Redis;
+
+namespace RedMist.TimingAndScoringService;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Logging.ClearProviders();
+        builder.Logging.AddNLog("NLog");
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(policy =>
+            {
+                policy.AllowAnyOrigin();
+                policy.AllowAnyHeader();
+            });
+        });
+
+        builder.Services.AddKeycloakWebApiAuthentication(builder.Configuration);
+        builder.Services.AddAuthorization().AddKeycloakAuthorization(options =>
+        {
+            options.EnableRolesMapping = RolesClaimTransformationSource.Realm;
+            // Note, this should correspond to role configured with KeycloakAuthenticationOptions
+            options.RoleClaimType = KeycloakConstants.RoleClaimType;
+        });
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Timing and Scoring Services", Version = "v1" });
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT Authorization header using the Bearer scheme."
+
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                      new OpenApiSecurityScheme
+                      {
+                          Reference = new OpenApiReference
+                          {
+                              Type = ReferenceType.SecurityScheme,
+                              Id = "Bearer"
+                          }
+                      }, []
+                }
+            });
+        });
+
+        string sqlConn = builder.Configuration["ConnectionStrings:Default"] ?? throw new ArgumentNullException("SQL Connection");
+
+        string redisConn = $"{builder.Configuration["REDIS_SVC"]},password={builder.Configuration["REDIS_PW"]}";
+        builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(redisConn, c => { c.AbortOnConnectFail = false; c.ConnectRetry = 10; c.ConnectTimeout = 10; }));
+
+        builder.Services.AddSingleton<IDateTimeHelper, DateTimeHelper>();
+
+        builder.Services.AddHealthChecks()
+            //.AddCheck<StartupHealthCheck>("Startup", tags: ["startup"])
+            .AddSqlServer(sqlConn, tags: ["db", "sql", "sqlserver"])
+            .AddRedis(redisConn, tags: ["cache", "redis"])
+            .AddProcessAllocatedMemoryHealthCheck(maximumMegabytesAllocated: 1024, name: "Process Allocated Memory", tags: ["memory"]);
+
+        builder.Services.AddSignalR(o => o.MaximumParallelInvocationsPerClient = 3)
+            .AddStackExchangeRedis(redisConn, options =>
+            {
+                options.Configuration.ChannelPrefix = RedisChannel.Literal("timing-scoring");
+            });
+
+        builder.Services.AddTransient<IDateTimeHelper, DateTimeHelper>();
+        //builder.Services.AddSingleton<StartupHealthCheck>();
+        //builder.Services.AddSingleton<ServiceTracking>();
+        //builder.Services.AddSingleton<HubConnectionContext>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            Console.Title = "Timing and Scoring Services";
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.MapHealthChecks("/healthz/startup", new HealthCheckOptions
+        {
+            Predicate = _ => true, // Run all checks
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        }).AllowAnonymous();
+        app.MapHealthChecks("/healthz/live", new HealthCheckOptions
+        {
+            Predicate = _ => false, // Only check that service is not locked up
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        }).AllowAnonymous();
+        app.MapHealthChecks("/healthz/ready", new HealthCheckOptions
+        {
+            Predicate = _ => true, // Run all checks
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        }).AllowAnonymous();
+
+        app.UseHttpsRedirection();
+        app.UseCors();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
+
+        app.MapHub<TimingAndScoringHub>("/ts-hub");
+
+        await app.RunAsync();
+    }
+}
