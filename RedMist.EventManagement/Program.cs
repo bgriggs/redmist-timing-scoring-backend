3ea2c70adb483b3287c44c457ffcd4406518d867
Brian Griggs <brian@bigmissionmotorsports.com>
2025-03-07 17:01:26 -0700

    Initial event management project.

---
+++ RedMist.EventManagement/Program.cs
@@ -0,0 +1,117 @@
+
+using HealthChecks.UI.Client;
+using Keycloak.AuthServices.Authentication;
+using Keycloak.AuthServices.Authorization;
+using Keycloak.AuthServices.Common;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using NLog.Extensions.Logging;
+
+namespace RedMist.EventManagement;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Logging.ClearProviders();
+        builder.Logging.AddNLog("NLog");
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(policy =>
+            {
+                policy.AllowAnyOrigin();
+                policy.AllowAnyHeader();
+            });
+        });
+
+        builder.Services.AddKeycloakWebApiAuthentication(builder.Configuration);
+        builder.Services.AddAuthorization().AddKeycloakAuthorization(options =>
+        {
+            options.EnableRolesMapping = RolesClaimTransformationSource.Realm;
+            // Note, this should correspond to role configured with KeycloakAuthenticationOptions
+            options.RoleClaimType = KeycloakConstants.RoleClaimType;
+        });
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Timing and Scoring Services", Version = "v1" });
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT Authorization header using the Bearer scheme."
+
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                  new OpenApiSecurityScheme
+                  {
+                      Reference = new OpenApiReference
+                      {
+                          Type = ReferenceType.SecurityScheme,
+                          Id = "Bearer"
+                      }
+                  }, []
+            }
+        });
+        });
+
+        string sqlConn = builder.Configuration["ConnectionStrings:Default"] ?? throw new ArgumentNullException("SQL Connection");
+        //builder.Services.AddDbContextFactory<TsContext>(op => op.UseSqlServer(sqlConn));
+        builder.Services.AddHealthChecks()
+            //.AddCheck<StartupHealthCheck>("Startup", tags: ["startup"])
+            .AddSqlServer(sqlConn, tags: ["db", "sql", "sqlserver"])
+            //.AddRedis(redisConn, tags: ["cache", "redis"])
+            .AddProcessAllocatedMemoryHealthCheck(maximumMegabytesAllocated: 7200, name: "Process Allocated Memory", tags: ["memory"]);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            Console.Title = "Event Management";
+            //app.MapOpenApi();
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.MapHealthChecks("/healthz/startup", new HealthCheckOptions
+        {
+            Predicate = _ => true, // Run all checks
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        }).AllowAnonymous();
+        app.MapHealthChecks("/healthz/live", new HealthCheckOptions
+        {
+            Predicate = _ => false, // Only check that service is not locked up
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        }).AllowAnonymous();
+        app.MapHealthChecks("/healthz/ready", new HealthCheckOptions
+        {
+            Predicate = _ => true, // Run all checks
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        }).AllowAnonymous();
+
+        app.UseHttpsRedirection();
+        app.UseCors();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
+
+        await app.RunAsync();
+    }
+}
